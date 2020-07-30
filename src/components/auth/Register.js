4077297e59657195b60c44afb694a88135fea75a
carloMeloni <carlo.meloni@hotmail.it>
2020-07-30 16:49:50 +0200

    Implementing Auth reducer and Register action

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -2,10 +2,11 @@ import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
 
-const Register = (props) => {
+const Register = ({setAlert, register}) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -23,9 +24,9 @@ const Register = (props) => {
     const onSubmit = async e => {
         e.preventDefault();
         if(password !== password2) {
-            props.setAlert('Cosa gazzu sei fendi?', 'danger')
+           setAlert('Passwords do not match', 'danger')
         } else {
-            console.log('success!');
+           register({ name, email, password });
         }
     }
 
@@ -36,14 +37,12 @@ const Register = (props) => {
             <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
             <form className="form" onSubmit={e => onSubmit(e)}>
                 <div className="form-group">
-                <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
+                <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)}  />
                 </div>
                 <div className="form-group">
-                <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required />
-                <small className="form-text"
-                    >This site uses Gravatar so if you want a profile image, use a
-                    Gravatar email</small
-                >
+                <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)}  />
+                <small className="form-text">This site uses Gravatar so if you want a profile image, use a
+                    Gravatar email</small>
                 </div>
                 <div className="form-group">
                 <input
@@ -51,7 +50,7 @@ const Register = (props) => {
                     placeholder="Password"
                     name="password"
                     value={password} onChange={e => onChange(e)}
-                    minLength="6"
+                    
                 />
                 </div>
                 <div className="form-group">
@@ -60,7 +59,7 @@ const Register = (props) => {
                     placeholder="Confirm Password"
                     name="password2"
                     value={password2} onChange={e => onChange(e)}
-                    minLength="6"
+                    
                 />
                 </div>
                 <input type="submit" className="btn btn-primary" value="Register" />
@@ -74,6 +73,7 @@ const Register = (props) => {
 
 Register.propTypes = {
     setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
 }
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
